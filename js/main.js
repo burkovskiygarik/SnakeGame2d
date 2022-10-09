e9142b708f643ed04946c7986e30fd4a28516612
Igor Burkovskii <burkovskiygarik@gmail.com>
2022-10-09 19:12:23 +0500

    first data

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById("game")
+const format = document.getContext("2d")
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const food = new Image()
+food.src = "img/apple.png"
+
+const field = 48;
+const score = 0;
+
+//дано 
+
